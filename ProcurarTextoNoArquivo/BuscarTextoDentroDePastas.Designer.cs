03e0077265041b54742c0f62a0b41eb1623f202a
Gabriel Rodrigues de Almeida <G.ALMEIDA@btp.com.br>
2019-09-05 13:29:37 -0300

    - master

---
+++ ProcurarTextoNoArquivo/BuscarTextoDentroDePastas.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace ProcurarTextoNoArquivo
+{
+    partial class BuscarTextoDentroDePastas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dtgArquivos = new System.Windows.Forms.DataGridView();
+            this.txtCaminho = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtProcura = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.NumeroLinha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Arquivo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Extensao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CaminhoArquivo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DePara = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgArquivos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.dtgArquivos);
+            this.groupBox2.Location = new System.Drawing.Point(12, 97);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(636, 462);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // dtgArquivos
+            // 
+            this.dtgArquivos.AllowUserToAddRows = false;
+            this.dtgArquivos.AllowUserToDeleteRows = false;
+            this.dtgArquivos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtgArquivos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dtgArquivos.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedHeaders;
+            this.dtgArquivos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgArquivos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NumeroLinha,
+            this.Arquivo,
+            this.Extensao,
+            this.Nome,
+            this.CaminhoArquivo,
+            this.DePara});
+            this.dtgArquivos.Location = new System.Drawing.Point(9, 16);
+            this.dtgArquivos.Name = "dtgArquivos";
+            this.dtgArquivos.ReadOnly = true;
+            this.dtgArquivos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgArquivos.Size = new System.Drawing.Size(621, 440);
+            this.dtgArquivos.TabIndex = 0;
+            this.dtgArquivos.DoubleClick += new System.EventHandler(this.DtgArquivos_DoubleClick);
+            // 
+            // txtCaminho
+            // 
+            this.txtCaminho.Location = new System.Drawing.Point(60, 13);
+            this.txtCaminho.Name = "txtCaminho";
+            this.txtCaminho.Size = new System.Drawing.Size(452, 20);
+            this.txtCaminho.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(555, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Pesquisar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Caminho:";
+            // 
+            // txtProcura
+            // 
+            this.txtProcura.Location = new System.Drawing.Point(60, 45);
+            this.txtProcura.Name = "txtProcura";
+            this.txtProcura.Size = new System.Drawing.Size(452, 20);
+            this.txtProcura.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Texto:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtProcura);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txtCaminho);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(636, 79);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // NumeroLinha
+            // 
+            this.NumeroLinha.DataPropertyName = "Linha";
+            this.NumeroLinha.HeaderText = "Numero da Linha";
+            this.NumeroLinha.Name = "NumeroLinha";
+            this.NumeroLinha.ReadOnly = true;
+            this.NumeroLinha.Width = 80;
+            // 
+            // Arquivo
+            // 
+            this.Arquivo.DataPropertyName = "NomeArquivo";
+            this.Arquivo.HeaderText = "Arquivo";
+            this.Arquivo.Name = "Arquivo";
+            this.Arquivo.ReadOnly = true;
+            this.Arquivo.Width = 68;
+            // 
+            // Extensao
+            // 
+            this.Extensao.DataPropertyName = "Extensao";
+            this.Extensao.HeaderText = "extensão arquivo";
+            this.Extensao.Name = "Extensao";
+            this.Extensao.ReadOnly = true;
+            this.Extensao.Width = 104;
+            // 
+            // Nome
+            // 
+            this.Nome.DataPropertyName = "TextoLinha";
+            this.Nome.HeaderText = "Texto da Linha";
+            this.Nome.Name = "Nome";
+            this.Nome.ReadOnly = true;
+            this.Nome.Width = 95;
+            // 
+            // CaminhoArquivo
+            // 
+            this.CaminhoArquivo.HeaderText = "Caminho Arquivo";
+            this.CaminhoArquivo.Name = "CaminhoArquivo";
+            this.CaminhoArquivo.ReadOnly = true;
+            this.CaminhoArquivo.Width = 103;
+            // 
+            // DePara
+            // 
+            this.DePara.HeaderText = "De -> Para";
+            this.DePara.Name = "DePara";
+            this.DePara.ReadOnly = true;
+            this.DePara.Width = 77;
+            // 
+            // BuscarTextoDentroDePastas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 571);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BuscarTextoDentroDePastas";
+            this.Text = "BuscarTextoDentroDePastas";
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgArquivos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtCaminho;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtProcura;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dtgArquivos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NumeroLinha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Arquivo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Extensao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CaminhoArquivo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DePara;
+    }
+}
---
+++ ProcurarTextoNoArquivo/BuscarTextoDentroDePastas.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProcurarTextoNoArquivo
+{
+    public partial class BuscarTextoDentroDePastas : Form
+    {
+        List<ArquivoEncontrado> linhasEncontradas;
+        public BuscarTextoDentroDePastas()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (ValidarCampos()) return;
+            linhasEncontradas = new List<ArquivoEncontrado>();
+            dtgArquivos.Rows.Clear();
+
+            var pastas = Directory.GetDirectories(txtCaminho.Text.Trim(), "*", SearchOption.AllDirectories);
+
+            pastas = pastas.Where(p => !p.EndsWith("bin") 
+            && !p.EndsWith("release") 
+            && !p.EndsWith("Properties")
+            && !p.EndsWith("obj")
+            && !p.EndsWith("Debug")
+
+            && !p.Contains("obj")
+            && !p.Contains("bin")
+
+            && !p.Contains(".svn")
+            && !p.Contains(".vs")
+            && !p.Contains("x86")
+            && !p.Contains("packages")
+            && !p.Contains("v16")).ToArray();
+
+            foreach (var pasta in pastas)
+            {
+                ProcurarArquivos(pasta, txtProcura.Text.Trim());
+            }
+
+            //dtgArquivos.DataSource = linhasEncontradas;
+            dtgArquivos.Refresh();
+        }
+
+        private bool ValidarCampos()
+        {
+            var notificacoes = new List<string>();
+
+            if (String.IsNullOrWhiteSpace(txtCaminho.Text))
+                notificacoes.Add("Caminho obrigatório para fazer a pesquisa");
+
+            if (String.IsNullOrEmpty(txtProcura.Text))
+                notificacoes.Add("Algum texto deve ser informado para pesquisar");
+
+            if (!string.IsNullOrWhiteSpace(txtProcura.Text) && !Directory.Exists(txtCaminho.Text.Trim()))
+                notificacoes.Add("Caminho informado não encontrado");
+
+            if (notificacoes.Any()) MessageBox.Show(notificacoes.Aggregate((A, B) => A + ", " + B), "Avisos", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            return notificacoes.Any();
+        }
+
+        private void ProcurarArquivos(string caminho, string parametro)
+        {
+            try
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(caminho);
+                FileInfo[] files = directoryInfo.GetFiles();
+
+                int atual = 0;
+                int total = files.Count();
+
+                string caminhoArquivo = string.Empty;
+                StringBuilder sb;
+
+                List<string> linhas;
+                string linhaAlterada;
+
+                foreach (var file in files)
+                {
+                    atual++;
+                    caminhoArquivo = file.FullName;
+
+                    Utilitarios.GeradorArquivo geradorArquivo = new Utilitarios.GeradorArquivo(caminhoArquivo);
+                    sb = geradorArquivo.LerArquivoTextoStringBuilder();
+
+                    linhas = TransformarEmLista(sb.ToString());
+                    linhaAlterada = string.Empty;
+
+                    foreach (var linha in linhas)
+                    {
+                        if (linha.ToUpper().Contains(parametro.ToUpper()))
+                        {
+                            //linhasEncontradas.Add(new ArquivoEncontrado(linha.Trim(), file.Name, linhas.IndexOf(linha) + 1, Path.GetExtension($"{caminho}\\{file}")));
+                            dtgArquivos.Rows.Add(file.Name, linhas.IndexOf(linha) + 1, Path.GetExtension($"{caminho}\\{file}"), linha.Trim(), $"{caminho}\\{file}", VerificarDePara(linha.ToUpper()));
+                            var teste = VerificarDePara(linha.ToUpper());
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+            string VerificarDePara(string linha)
+            {
+                switch (VerificarContains(linha))
+                {
+                    case "ITEM": return "ITEM -> MERCADORIA";
+                    case "VESSEL_DETAILS": return "VESSEL_DETAILS -> ESCALA";
+                    case "VESSELDETAILS": return "VESSEL_DETAILS -> ESCALA";
+                    case "VESVOYAGE": return "VES_VOYAGE -> ESCALA";
+                    case "VES_VOYAGE": return "VES_VOYAGE -> ESCALA";
+                    case "VESSEL": return "VESSEL -> NAVIO";
+                    case "ITEM_REEFER": return "ITEM_REEFER -> MERCADORIA";                    
+                    case "ITEM_SEAL": return "ITEM_SEAL -> AMR_LACRE_MERCADORIA";
+                    case "ITEMSEAL": return "ITEM_SEAL -> AMR_LACRE_MERCADORIA";
+                    case "ITEM_OOG": return "ITEM_OOG -> EXCESSO";
+                    case "ITEMOOG": return "ITEM_OOG -> EXCESSO";
+                    case "ITEM_LOCATION": return "ITEM_LOCATION -> AMR_LOCALIZACAO_MERCADORIA";
+                    case "ITEMLOCATION": return "ITEM_LOCATION -> AMR_LOCALIZACAO_MERCADORIA";
+                    case "BOOKINGS": return "BOOKINGS -> BOOKING + ITEM_BOOKING";
+                    case "PORTCODE": return "PORTCODE -> PORTO";
+                    default: return "";
+                }
+                
+            }
+
+            string VerificarContains(string linha)
+            {
+                var tabelasTops = new List<string>
+                {
+                    "VESVOYAGE",
+                    "VES_VOYAGE",
+                    "BOOKINGS",
+                    "ITEM_REEFER",
+                    "ITEM_SEAL",
+                    "ITEMSEAL",
+                    "ITEM_LOCATION",
+                    "ITEMLOCATION",
+                    "ITEM_OOG",
+                    "ITEMOOG",
+                    "ITEM",
+                    "VESSEL_DETAILS",
+                    "ITEM_REEFER",
+                    "VESSEL",
+                    "PORTCODE"
+                };
+
+                foreach (var tabelaTops in tabelasTops)
+                {                                                                                                                                              
+                    if (linha.Contains(tabelaTops))
+                        return tabelaTops;
+                }
+
+                return "";
+            }
+        }
+
+        private List<string> TransformarEmLista(string texto)
+        {
+
+            return texto.Replace("\n", "").Split('\r').ToList();
+        }
+
+        private void DtgArquivos_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {                
+                int indice = dtgArquivos.SelectedRows[0].Index;
+                if (indice > -1)
+                {
+                    string arquivo = dtgArquivos.Rows[indice].Cells["CaminhoArquivo"].Value.ToString();
+                    try
+                    {
+                        System.Diagnostics.Process.Start("notepad++", arquivo);
+                    }
+                    catch (Win32Exception)
+                    {
+                        System.Diagnostics.Process.Start("notepad", arquivo);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+
+    public class ArquivoEncontrado
+    {
+        public ArquivoEncontrado(string textoLinha, string nomeArquivo, int linha, string extensao)
+        {
+            TextoLinha = textoLinha;
+            NomeArquivo = nomeArquivo;
+            Linha = linha;
+            Extensao = extensao;
+        }
+
+        public string TextoLinha { get; set; }
+        public string NomeArquivo { get; set; }
+        public int Linha { get; set; }
+        public string Extensao { get; set; }
+    }
+}
